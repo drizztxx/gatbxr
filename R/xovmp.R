9dc18c16e47b624c8b1ef5baffe33d9cdac1716b
David Zhao <dzhao26@its.jnj.com>
2016-06-16 11:22:46 +0800

    combine crossover fucntionfs

--- R/xovmp.R
+++ R/xovmp.R
@@ -5,10 +5,10 @@
 #' representation of the individuals in the current population,
 #' applying crossover to consecutive pairs of individuals with
 #' defined probability and returns the resulting population.
-#' @aliases xovmp xovsp
+#' @aliases xovmp xovsp xovdp xovsh xovsprs xovdprs xovshrs
 #'
 #' @usage
-#' xovmp(OldChrom, Px = 0.7, Npt = 0, Rs = TRUE)
+#' xovmp(OldChrom, Px = 0.7, Npt = 0, Rs = FALSE)
 #' 
 #' @param OldChrom  a matrix containing the chromosomes of the old
 #' population. Each row corresponds to one individual.
@@ -20,16 +20,53 @@
 #' Default is set to 0.
 #' @param Rs  an optional logical value indicating whether or not to 
 #' force the production of offspring different from their parents.
-#' Default is set to TRUE.
+#' Default is set to FALSE.
 #' @return
 #' a matrix containing the offspring after mating, 
 #' ready to be mutated and/or evaluated, in the same 
 #' format as OldChrom.
 #' @export
 #' @author 
-#' The original matlab implementation of mutate was written by Carlos Fonseca and
+#' The original matlab implementation was written by Carlos Fonseca and
 #' updated by Andrew Chipperfield.
 #' The R implementation was written by David Zhao. 
+#' @details
+#' \code{xovmp} is the general crossover function for binary representation and 
+#' can be called by all other crossover functions. When \code{xovmp} called by 
+#' \code{\link{recombin}}, high-level crossover function, its arguments can aslo
+#' be passed on to. With different combination of argumetents, it can be used identically
+#' to \code{xovsp}, \code{xovdp}, \code{xovsh}, \code{xovsprs}, \code{xovdprs} and 
+#' \code{xovshrs}. Althogh these replicatical use, other form of crossover funcions
+#' are maintained for completence reason.
+#' \cr\cr
+#' \code{xovsp} performs single-point crossover between pairs of individuals. When called by
+#' \code{\link{recombin}}, \code{recombin("xovsp",Chrom)} is identical to 
+#' \code{recombin("xovmp",Chrom,Npt=1)}.
+#' \cr\cr
+#' \code{xovdp} performs double-point crossover between pairs of individuals. When called by
+#' \code{\link{recombin}}, \code{recombin("xovdp",Chrom)} is identical to 
+#' \code{recombin("xovmp",Chrom,Npt=2)}.
+#' \cr\cr
+#' \code{xovsh} performs shuffle-point crossover between pairs of individuals. When called by
+#' \code{\link{recombin}}, \code{recombin("xovsh",Chrom)} is identical to 
+#' \code{recombin("xovmp",Chrom)}.
+#' \cr\cr
+#' \code{xovsprs} performs single-point reduced surrogate crossover between 
+#' pairs of individuals. When called by \code{\link{recombin}}, 
+#' \code{recombin("xovsprs",Chrom)} is identical to 
+#' \code{recombin("xovmp",Chrom,Npt=1,Rs=TRUE)}.
+#' \cr\cr
+#' \code{xovdprs} performs double-point reduced surrogate crossover between 
+#' pairs of individuals. When called by \code{\link{recombin}}, 
+#' \code{recombin("xovdprs",Chrom)} is identical to 
+#' \code{recombin("xovmp",Chrom,Npt=2,Rs=TRUE)}.
+#' \cr\cr
+#' \code{xovshrs} performs shuffle-point reduced surrogate crossover between 
+#' pairs of individuals. When called by \code{\link{recombin}}, 
+#' \code{recombin("xovshrs",Chrom)} is identical to 
+#' \code{recombin("xovmp",Chrom,Rs=TRUE)}.
+#' @seealso
+#' \code{\link{recombin}}
 #' @examples
 #' 
 #' Chrom = crtbp(40,10)$Chrom
@@ -40,7 +77,7 @@
 xovmp <- function(OldChrom,
                   Px=0.7,
                   Npt=0,
-                  Rs=TRUE){
+                  Rs=FALSE){
   ## Identify the population size (Nind) and the chromosome length (Lind)
   Nind <- NROW(OldChrom); Lind <- NCOL(OldChrom)
   
@@ -52,7 +89,7 @@ xovmp <- function(OldChrom,
   even <- (1:Nind)[!as.logical(1:Nind %% 2)]
   
   ## Compute the effective length of each chromosome pair
-  Mask <- outer(OldChrom[odd,] != OldChrom[even,],Rs,"|")
+  Mask <- outer(OldChrom[odd,] != OldChrom[even,],!Rs,"|")
   if(Xops==1) Mask <- t(Mask)
   Mask <- t(apply(Mask,1,cumsum))
   
@@ -100,6 +137,51 @@ xovmp <- function(OldChrom,
 xovsp <- function(OldChrom,
                   Px=0.7){
   ## call low level function with appropriate parameters
+  NewChrom <- xovmp(OldChrom,Px,1,FALSE)
+  return(NewChrom)
+}
+
+#' @rdname xovmp
+#' @export
+xovdp <- function(OldChrom,
+                  Px=0.7){
+  ## call low level function with appropriate parameters
+  NewChrom <- xovmp(OldChrom,Px,2,FALSE)
+  return(NewChrom)
+}
+
+#' @rdname xovmp
+#' @export
+xovsh <- function(OldChrom,
+                  Px=0.7){
+  ## call low level function with appropriate parameters
+  NewChrom <- xovmp(OldChrom,Px,0,FALSE)
+  return(NewChrom)
+}
+
+#' @rdname xovmp
+#' @export
+xovsprs <- function(OldChrom,
+                  Px=0.7){
+  ## call low level function with appropriate parameters
   NewChrom <- xovmp(OldChrom,Px,1,TRUE)
+  return(NewChrom)
+}
+
+#' @rdname xovmp
+#' @export
+xovdprs <- function(OldChrom,
+                    Px=0.7){
+  ## call low level function with appropriate parameters
+  NewChrom <- xovmp(OldChrom,Px,2,TRUE)
+  return(NewChrom)
+}
+
+#' @rdname xovmp
+#' @export
+xovshrs <- function(OldChrom,
+                    Px=0.7){
+  ## call low level function with appropriate parameters
+  NewChrom <- xovmp(OldChrom,Px,0,TRUE)
   return(NewChrom)
 }
