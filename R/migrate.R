9fa2f0d3198fd2f02ce245d510d0aa1d7a017ccd
David Zhao <dzhao26@its.jnj.com>
2016-06-22 16:03:42 +0800

    recmut and migrate added

---
+++ R/migrate.R
@@ -0,0 +1,166 @@
+#' @title Migration of individuals between subpopulations
+#' @description
+#' This function performs migration of individuals.
+#' @usage
+#' migrate(Chrom, SUBPOP = 1, ObjV, MIGR = 0.2, Select = FALSE, 
+#' Structure = c("net","neighbourhood","ring"))
+#' @param Chrom a matrix containing the individuals of the current
+#' population. Each row corresponds to one individual.
+#' @inheritParams ranking
+#' @param MIGR a number indicating rate of individuals to be 
+#' migrated per subpopulation. Default is set to 0.2.
+#' @param Select an logical value indicating whether fitness-based selection
+#' will be used. Default is set to FALSE. If set to TRUE, then \code{ObjV} must also 
+#' be defined.
+#' @param Structure an character string indicating the name of structure of the subpopulations 
+#' for migration. The default structure "net" indicates net structure (unconstrained 
+#' migration).
+#' @details
+#' \pkg{gatbxr} provides support for multiple subpopulations through the use of 
+#' high-level genetic operator functions and a routine for exchanging individuals
+#' between subpopulations. In the literature, the use of multiple populations has
+#' been shown, in most cases, to improve the quality of the results obtained using GAs
+#' compared to the single population GA.
+#' \cr\cr
+#' \pkg{gatbxr} supports the use of a single population divided into a number of 
+#' subpopulations or \eqn{demes} by modifying the use of data structures such that 
+#' subpopulations are stored in contiguous blocks wihtin a single matrix.
+#' \cr\cr
+#' This model enables each subpopulation evolved over generations by a traditional GA and 
+#' from time to time individuals migrate from one subpopulation to another. The amount of 
+#' migration of individuals and the pattern of that migration determines how much genetic
+#' diversity can occur.
+#' \cr\cr
+#' \code{migrate} implements the transfer of individuals between subpopulations, in which
+#' \code{Structure} specifies the population topology over which migration will tha place.
+#' The default structure unrestricted migration topology (\code{net}) is the most general 
+#' migration strategy. Here, individuals may migrate from any subpopulation to another. For each 
+#' subpopulation, a pool of potential immigrants is constructed from the other subpopulations.
+#' The individual migrants are then determined according to the appropriate selection
+#' strategy.
+#' \cr\cr
+#' Structure \code{ring} transfers individuals between directionally adjacent subpopulations.
+#' And \code{neighbourhood} is a similar strategy. Like the \code{ring} topology, migration
+#' is made only between nearest neighbours, however, migration may occur in either direction
+#' between subpopulations. For each subpopulation, the possible immigrants are determined,
+#' according to the desired selection method, from the adjacent subpopulations and a final
+#' selection made from this pool of individuals. This ensures that individuals will not 
+#' migrate from a subpopulation to the same population.
+#' @return a list containing following components:
+#' \item{Chrom}{a matrix containing the individuals of the current
+#'  population after reinsertion.}
+#' \item{ObjV}{if ObjV is input parameters, then return 
+#'  vector containing the objective values of the individuals
+#'  of the current generation after reinsertion. Else return NULL.}
+#' @seealso
+#' \code{\link{select}}, \code{\link{recombin}}, \code{\link{mutate}},
+#' \code{\link{reins}}     
+#' @export
+#' @author 
+#' The original matlab implementation of reins was written by Hartmut Pohlheim.
+#' The R implementation was written by David Zhao.  
+#' @examples
+#' ## create initial population
+#' Chrom = crtbp(50,10)
+#' 
+#' ## calculate objective value with sum function
+#' objv = apply(Chrom,1,sum)
+#' 
+#' ## assume there are 5 subpopulations, then perform ranking
+#' ObjV = ranking(objv, SUBPOP = 5)
+#' 
+#' ## chosse 20% of the individuals of one subpopulation and replaces
+#' ## these individuals with the fittest individuals from an adjacent
+#' ## subpopulation in a unidirectional ring structure
+#' res = migrate(Chrom,SUBPOP = 5, ObjV, Select = TURE, Structure = "ring")
+
+migrate <- function(Chrom, SUBPOP = 1, ObjV, MIGR = 0.2, Select = FALSE, 
+                    Structure = c("net","neighbourhood","ring")){
+  ## Check parameter consistency
+  Nind <- NROW(Chrom); Nvar <- NCOL(Chrom)
+  if (length(SUBPOP) != 1) {
+    stop("SUBPOP must be a scalar")
+  } 
+  if(SUBPOP == 1 || MIGR == 0){
+    if (missing(ObjV)) ObjV <- NULL
+    return(list(Chrom = Chrom, ObjV = ObjV))
+  }
+  if (Nind %% SUBPOP) stop("Chrom and SUBPOP disagree")
+  NIND <- Nind/SUBPOP
+  
+  if (!missing(ObjV)){
+    if (!is.vector(ObjV)) stop("ObjV must be a vector")
+    mO <- NROW(ObjV)
+    if (Nind != mO) stop("Chrom and ObjV disagree")
+  }
+  
+  if (MIGR < 0 || MIGR > 1) {
+    MIGR <- 0.2
+    warning("Parameter for migration rate must be a scalar in [0, 1] and is temporarily
+            set to default 0.2")
+  }
+  
+  if (!Structure %in% c("net","neighbourhood","ring")){
+    Structure <- "net"
+    warning("Strucutre name illeagal, and this value is temporarily set to
+            the default net")
+  }
+  
+  if (Select && missing(ObjV)) {
+    Select <- FALSE
+    warning("ObjV for fitness-based migration needed, Select has temporarily set to FALSE")
+  }
+  
+  MigTeil <- max(floor(NIND * MIGR), 1) ## number of individuals to migrate
+  
+  ## Perform migration between subpopulations --> create a matrix for migration
+  ## in every subpopulation from best individuals of the other subpopulations
+  ChromMigAll <- NULL
+  if (!missing(ObjV)) ObjVAll <- NULL
+  for(x in 1:SUBPOP){
+    ## sort ObjV of actual subpopulation
+    if (Select) {
+      IndMigSo <- order(ObjV[((x-1)*NIND+1):(x*NIND)])
+    } else IndMigSo <- order(runif(NIND))
+    ## take MigTeil (best) individuals, copy individuals and objective values
+    IndMigTeil <- IndMigSo[1:MigTeil] + (x-1)*NIND
+    ChromMigAll <- rbind(ChromMigAll, Chrom[IndMigTeil,])
+    if (!missing(ObjV)) ObjVAll <- c(ObjVAll,ObjV[IndMigTeil])
+  }
+  
+  ## perform migration
+  for (i in 1:SUBPOP){
+    ChromMig <- ChromMigAll
+    if (!missing(ObjV)) ObjVMig <- ObjVAll
+    if (Structure == "neighbourhood"){
+      ## select individuals of neighbourhood subpopulations for ChromMig and ObjVMig
+      popnum <- c(SUBPOP,1:SUBPOP,1)
+      ins1 <- popnum[i]; ins2 <- popnum[i+2]
+      InsRows <- c(((ins1-1)*MigTeil+1):ins1*MigTeil,
+                   ((ins2-1)*MigTeil+1):ins2*MigTeil)
+      ChromMig <- ChromMig[InsRows,]
+      if (!missing(ObjV)) ObjVMig <- ObjVMig[InsRows]
+    } else if (Structure == "ring"){
+      ## select individuals of actual-1 subpopulation for ChromMig and ObjVMig
+      popnum <- c(SUBPOP,1:SUBPOP,1)
+      ins1 <- popnum[i]
+      InsRows <- ((ins1-1)*MigTeil+1):ins1*MigTeil
+      ChromMig <- ChromMig[InsRows,]
+      if (!missing(ObjV)) ObjVMig <- ObjVMig[InsRows]    
+    } else {
+      ## delete individuals of actual subpopulation from ChromMig and ObjVMig
+      DelRows <- ((i-1)*MigTeil+1):i*MigTeil
+      ChromMig <- ChromMig[-DelRows,]
+      if (!missing(ObjV)) ObjVMig <- ObjVMig[-DelRows]
+    }
+    ## Create an index from a sorted vector with random numbers
+    IndMigRa <- order(runif(NROW(ChromMig)))
+    ## Take MigTeil numbers from the random vector
+    IndMigN <- IndMigRa[1:MigTeil]
+    ## copy MigTeil individuals into Chrom and ObjV
+    Chrom[(1:MigTeil)+(i-1)*NIND,] <- ChromMig[IndMigN,]
+    if (!missing(ObjV)) ObjV[(1:MigTeil)+(i-1)*NIND] <- ObjVMig[IndMigN]
+  }
+  if (missing(ObjV)) ObjV <- NULL
+  return(list(Chrom = Chrom, ObjV = ObjV))
+}
---
+++ R/recmut.R
@@ -0,0 +1,143 @@
+#' @title Line recombination with mutation features
+#' 
+#' @description
+#' This function performs line recombination with mutation features between
+#' pairs of individuals.
+#' @usage
+#' recmut(OldChrom, FieldDR, MutOpt = NULL)
+#' @inheritParams mutate
+#' @param MutOpt  an optional vector containing mutation rate and shrink value:
+#' \enumerate{
+#'   \item MutOpt[1] a number containing the recombination rate in the range \eqn{[0, 1]}.
+#' By default this value is set to 1.
+#'   \item MutOpt[2] a number for shrinking the
+#' mutation range in the range \eqn{[0, 1]}. 
+#' By default this vaule is assumed to 1 (no shrinking).
+#' }
+#' @details
+#' \code{recmut} is a function only applicable to populations of real-value variables (
+#' and not binary or integer). The pairs are mated in order, odd row with the next even row.
+#' If the number of rows in the matrix \code{OldChrom} is odd the the last row is not 
+#' mated and added at the end of \code{NewChrom}. The population should therefore be organised 
+#' into contiguous pairs that require mating. This can be achieved by using the function
+#' \code{\link{ranking}} to assign a fitness level to each individual and a selection function
+#' (e.g. \code{\link{select}}) to select individuals with a probability related to their
+#' fitness in the current population.
+#' \cr\cr
+#' The offsprings of a pair of two parents are computed as follows:\cr
+#' \eqn{offsping1 = parent1 + RecMx * range * Mutopt[2] * delta *Diff}\cr
+#' \eqn{offsping2 = parent2 + RecMx * range * Mutopt[2] * delta * -Diff}\cr
+#' where,\cr \eqn{MutMx} = 1 or -1 with probability \eqn{MutOpt[1]}, (- with 
+#' probability 0.9) else 0. \cr
+#' \eqn{range = 0.5 *} domain of variable (search interval defined
+#' by \code{FieldDR}). \cr
+#' \eqn{delta = sum(0 to m-1)pi*2^-i, pi = 1} wiht probability 
+#' \eqn{1/m}, else 0, m = 20.\cr
+#' \eqn{Diff = (parent2 - parent1)/absolute(parent1 - parent2)}.
+#' \cr\cr
+#' The recombiantion operator \code{recmut} generates offspring in a direction defined by
+#' the parents (line recombination). It tests more often outside the area defined by the
+#' parents and in the direction of parent1. The point for the offspring is defined by
+#' features of the mutation operator. The probability of small step sizes is greater than
+#' that of bigger steps (see \code{\link{mutbga}}).
+#' @seealso
+#' \code{\link{mutate}}, \code{\link{mutbga}}, \code{\link{reclin}}
+#' @return
+#' a matrix containing the chromosomes of the population
+#' after mating, ready to be mutated and/or evaluated,
+#' in the same format as OldChrom.
+#' @export
+#' @note
+#' This function doesn't work with high level recombination function \code{\link{recombin}}
+#' @author 
+#' The original matlab implementation of crtrp was written by Hartmut Pohlheim. 
+#' The R implementation was written by David Zhao. 
+#' @examples
+#' ## Initial a real-valued population
+#' FieldDR = matrix(c(-100,-50,-30,-20,100,50,30,20),2,4,byrow=T) 
+#' Chrom = crtrp(6,FieldDR)
+#' 
+#' ## Perform line recombination with mutation
+#' NewChrom = recmut(Chrom,FieldDR)
+
+recmut <- function(OldChrom,FieldDR,MutOpt = NULL){
+  
+  ## Parameter consistency check
+  Nind <- NROW(OldChrom); Nvar <- NCOL(OldChrom)
+  
+  mF <- NROW(FieldDR); nF <- NCOL(FieldDR)
+  if (mF != 2) stop("FieldDR must be a matrix with 2 rows")
+  if (Nvar != nF) stop("FieldDR and OldChrom disagree")
+  
+  if (is.null(MutOpt)) {
+    MutR <- 1; MutShrink <- 1
+  } else if (length(MutOpt) == 1) {
+    MutR <- MutOpt; MutShrink <- 1
+  } else if (length(MutOpt) == 2){
+    MutR <- MutOpt[1]; MutShrink <- MutOpt[2]
+  } else stop("Too many parameters in MutOpt")
+  
+  if (MutR < 0 || MutR > 1) {
+    MutR <- 1
+    warning("Parameter for recombination rate must be in [0, 1], this parameter has
+            been temporarily set to 1")
+  }
+  
+  if (MutShrink < 0 || MutShrink > 1) {
+    MutShrink <- 1
+    warning("Parameter for shrinking mutation range must be in [0, 1], this parameter has
+            been temporarily set to 1, which means no shrinking")
+  }  
+  
+  ## Identify the number of matings
+  Xops <- floor(Nind/2)
+  
+  ## perform mutation
+  ## get range matrix for every variable
+  Range <- as.vector(0.5 * MutShrink * diff(FieldDR))
+  upper <- matrix(rep(FieldDR[2,],Nind),Nind,Nvar,byrow = T)
+  lower <- matrix(rep(FieldDR[1,],Nind),Nind,Nvar,byrow = T)
+  
+  ## zeros and ones for mutate or not this variable, together with Range
+  Range <- do.call(rbind,lapply(1:Xops,function(x) Range * (runif(Nvar) < MutR)))
+  
+  ## compute, if + or - sign
+  Range <- Range * (1 - 2 * (matrix(runif(Xops*Nvar),Xops,Nvar) < 0.9))  
+  
+  ## compute distance between mating pairs
+  NormO <- sapply(1:Xops,function(x) abs(norm(as.matrix(OldChrom[2*x,]),type="2") -
+                    norm(as.matrix(OldChrom[2*x - 1,]),type="2")))
+  
+  ## compute difference between variables divided by distance
+  ChromDiff <- do.call(rbind,lapply(1:Xops,function(x) diff(OldChrom[(2*x-1):(2*x),])/NormO[x]))
+  
+  ## compute delta value for all individuals
+  ACCUR <- 20
+  Vect <- 2 ^ (-1 * (0:(ACCUR-1)))
+  Delta <- (matrix(runif(Xops*ACCUR),Xops,ACCUR) < 1/ACCUR) %*% Vect
+  Delta <- matrix(rep(Delta,Nvar),Xops,Nvar)
+  
+  ## perform recombination
+  NewChrom <- matrix(NA,Nind,Nvar)
+  odd <- (1:(Nind-1))[as.logical(1:(Nind-1) %% 2)]
+  even <- (1:Nind)[!as.logical(1:Nind %% 2)]
+  NewChrom[odd,] <- OldChrom[odd,] + Range * Delta * ChromDiff
+  NewChrom[even,] <- OldChrom[even,] + Range * Delta * ChromDiff
+  
+  ## If the number of individuals is odd, the last individual cannot be mated
+  ## but must be included in the new population
+  if(Nind %% 2){
+    NewChrom[Nind,] <- OldChrom[Nind,]
+  }
+  
+  ## Ensure variables boundaries, compare with lower and upper boundaries
+  upper.logic <- NewChrom > upper
+  lower.logic <- NewChrom < lower
+  if (any(upper.logic)){
+    NewChrom <- NewChrom * !upper.logic + upper * upper.logic
+  }
+  if (any(lower.logic)){
+    NewChrom <- NewChrom * !lower.logic + lower * lower.logic
+  }  
+  return(NewChrom)
+}
